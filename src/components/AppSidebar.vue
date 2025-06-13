9a147704d3e3b5aadb7272418b9bb1ff1c10403d
Haniel Hernández <haniel1121@outlook.com>
2025-06-13 08:43:16 -0600

    Added sidebar

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  // SidebarHeader,
+} from '@/components/ui/sidebar'
+import { ScrollTextIcon } from 'lucide-vue-next';
+</script>
+
+<template>
+  <Sidebar>
+    <div class="flex h-15 items-center border-b px-4 gap-2">
+      <ScrollTextIcon />
+      <h2 class="font-semibold "> Custom Invoice creator </h2>
+    </div>
+    <SidebarContent>
+      <SidebarGroup />
+      <SidebarGroup />
+    </SidebarContent>
+    <SidebarFooter />
+  </Sidebar>
+</template>
--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -1,13 +1,17 @@
-<script setup lang="ts"></script>
-<template lang="">
-  <div class="class flex flex-col h-screen">
-    <div class="border px-4 py-4">
-      <Button></Button>
+<script setup lang="ts">
+import { SidebarProvider } from '@/components/ui/sidebar'
+import AppSidebar from './AppSidebar.vue';
+import AppHeader from './AppHeader.vue';
+</script>
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <div class="flex flex-col w-full overflow-auto ">
+      <AppHeader />
+      <main class="p-4 md:px-6  bg-neutral-100 h-full">
+        <slot></slot>
+      </main>
     </div>
-    <div class="bg-neutral-100 h-full ">
-      <slot></slot>
-    </div>
-
-  </div>
+  </SidebarProvider>
 </template>
 <style lang="scss"></style>
