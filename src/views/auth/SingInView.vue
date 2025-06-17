fb5d2f202857645531ae07885103ba4bd487361f
Haniel Hernández <haniel1121@outlook.com>
2025-06-17 10:53:40 -0600

    added errors for singin view

--- src/views/auth/SingInView.vue
+++ src/views/auth/SingInView.vue
@@ -17,9 +17,12 @@ import { auth } from '@/lib/firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useRouter } from 'vue-router';
 import { ref } from 'vue';
-import { EyeClosed, EyeIcon } from 'lucide-vue-next';
+import { AlertCircle, EyeClosed, EyeIcon } from 'lucide-vue-next';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
 const router = useRouter()
+
+const error = ref<string | null>(null)
 
 const validationSchema = toTypedSchema(z.object({
     email: z.string().email(),
@@ -33,6 +36,7 @@ const { handleSubmit, isSubmitting } = useForm({
 })
 
 const onSubmit = handleSubmit(async ({ email, password }) => {
+    error.value = null
 
     try {
         await signInWithEmailAndPassword(auth, email, password)
@@ -40,60 +44,72 @@ const onSubmit = handleSubmit(async ({ email, password }) => {
 
     } catch (e) {
         console.error(e)
+        error.value = e as string
     }
 })
 </script>
 <template>
-    <Card class="w-full max-w-120">
+    <div class="flex flex-col gap-4 w-full max-w-120">
+        <Alert variant="destructive" v-if="error">
+            <AlertCircle class="w-4 h-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+                {{ error }}
+            </AlertDescription>
+        </Alert>
+        <Card class="w-full max-w-120">
 
-        <CardHeader class="text-center">
-            <CardTitle>Welcome back</CardTitle>
-            <CardDescription>Enter your credentials to access your account</CardDescription>
-        </CardHeader>
-        <CardContent>
+            <CardHeader class="text-center">
+                <CardTitle>Welcome back</CardTitle>
+                <CardDescription>Enter your credentials to access your account</CardDescription>
+            </CardHeader>
+            <CardContent>
 
-            <form @submit.prevent="onSubmit" class="flex flex-col gap-4">
-                <FormField v-slot="{ componentField }" name="email">
-                    <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                            <Input type="email" placeholder="name@example.com" v-bind="componentField" />
-                        </FormControl>
+                <form @submit.prevent="onSubmit" class="flex flex-col gap-4">
+                    <FormField v-slot="{ componentField }" name="email">
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type="email" placeholder="name@example.com" v-bind="componentField" />
+                            </FormControl>
 
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-                <FormField v-slot="{ componentField }" name="password">
-                    <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                            <div class="relative ">
-                                <Input :type="isPasswordVisible ? 'text' : 'password'" class="pr-10"
-                                    placeholder="Your password" v-bind="componentField" />
-                                <Button variant="ghost" @click="isPasswordVisible = !isPasswordVisible" type="button"
-                                    class="absolute top-0 right-0">
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+                    <FormField v-slot="{ componentField }" name="password">
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <div class="relative ">
+                                    <Input :type="isPasswordVisible ? 'text' : 'password'" class="pr-10"
+                                        placeholder="Your password" v-bind="componentField" />
+                                    <Button variant="ghost" @click="isPasswordVisible = !isPasswordVisible"
+                                        type="button" class="absolute top-0 right-0">
 
-                                    <EyeClosed v-if="isPasswordVisible" />
-                                    <EyeIcon v-else />
-                                </Button>
-                            </div>
-                        </FormControl>
+                                        <EyeClosed v-if="isPasswordVisible" />
+                                        <EyeIcon v-else />
+                                    </Button>
+                                </div>
+                            </FormControl>
 
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
 
-                <Button type="submit" size="lg">
-                    Sign In <span class="animate-spin border-4 border-neutral-300 border-t-white w-6 h-6 rounded-full "
-                        v-if="isSubmitting"> </span>
-                </Button>
+                    <Button type="submit" size="lg">
+                        Sign In <span
+                            class="animate-spin border-4 border-neutral-300 border-t-white w-6 h-6 rounded-full "
+                            v-if="isSubmitting"> </span>
+                    </Button>
 
-            </form>
-        </CardContent>
+                </form>
+            </CardContent>
 
 
 
-    </Card>
+        </Card>
+    </div>
+
 
 </template>
 <style lang="scss"></style>
