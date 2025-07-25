aedf505c7291a20d73908dd1bd1cb0f8f44ed718
Haniel Hernández <haniel1121@outlook.com>
2025-07-24 21:10:57 -0600

    added link to home on dashboard

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
-import SidebarTrigger from './ui/sidebar/SidebarTrigger.vue';
-
+import SidebarTrigger from './ui/sidebar/SidebarTrigger.vue'
 </script>
 <template>
     <Header class="h-16 border-b flex items-center justify-start px-4 md:px-6">
         <SidebarTrigger class="md:hidden mr-2" />
-        <span class="text-2xl font-semibold tracking-tight">Dashboard</span>
+        <router-link to="dashboard" class="text-2xl font-semibold tracking-tight">
+            Dashboard</router-link
+        >
     </Header>
 </template>
 <style lang="scss"></style>
--- src/components/InvoicesTable.vue
+++ src/components/InvoicesTable.vue
@@ -1,81 +1,100 @@
 <script lang="ts" setup>
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { searchClient } from '@/lib/algolia';
-import { Card, CardContent, CardHeader } from './ui/card';
-import CardTitle from './ui/card/CardTitle.vue';
-import CardDescription from './ui/card/CardDescription.vue';
-import Input from './ui/input/Input.vue';
-import { computed, ref, useTemplateRef } from 'vue';
-import CardFooter from './ui/card/CardFooter.vue';
-import { Button } from './ui/button';
-import { ChevronLeft, ChevronRight, EditIcon, EyeIcon,  SearchIcon, Trash } from 'lucide-vue-next';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow
+} from '@/components/ui/table'
+import { searchClient } from '@/lib/algolia'
+import { Card, CardContent, CardHeader } from './ui/card'
+import CardTitle from './ui/card/CardTitle.vue'
+import CardDescription from './ui/card/CardDescription.vue'
+import Input from './ui/input/Input.vue'
+import { computed, ref, useTemplateRef } from 'vue'
+import CardFooter from './ui/card/CardFooter.vue'
+import { Button } from './ui/button'
+import { ChevronLeft, ChevronRight, EditIcon, EyeIcon, SearchIcon, Trash } from 'lucide-vue-next'
 import dayjs from 'dayjs'
-import InvoIceDeleteDialog from './InvoIceDeleteDialog.vue';
-import { useInvoiceStore } from '@/stores/InvoiceStore';
-import { toast } from 'vue-sonner';
-import { auth } from '@/lib/firebase';
-import { Select, SelectContent, SelectTrigger, SelectValue } from './ui/select';
-import SelectItem from './ui/select/SelectItem.vue';
-import { useRole } from '@/composable/useRole';
-import { history as historyRouter } from 'instantsearch.js/es/lib/routers';
-import { singleIndex as singleIndexMapping } from 'instantsearch.js/es/lib/stateMappings';
-import Label from './ui/label/Label.vue';
+import InvoIceDeleteDialog from './InvoIceDeleteDialog.vue'
+import { useInvoiceStore } from '@/stores/InvoiceStore'
+import { toast } from 'vue-sonner'
+import { auth } from '@/lib/firebase'
+import { Select, SelectContent, SelectTrigger, SelectValue } from './ui/select'
+import SelectItem from './ui/select/SelectItem.vue'
+import { useRole } from '@/composable/useRole'
+import { history as historyRouter } from 'instantsearch.js/es/lib/routers'
+import { singleIndex as singleIndexMapping } from 'instantsearch.js/es/lib/stateMappings'
+import Label from './ui/label/Label.vue'
 
 const routing = {
     router: historyRouter(),
     stateMaping: singleIndexMapping('invoices')
 }
 const sortBy = useTemplateRef('sortBy')
-const flow = ref("asc")
+const flow = ref('asc')
 const openDeleteModal = ref(false)
 const invoiceId = ref<string | null>(null)
 const invoiceStore = useInvoiceStore()
 const { role } = useRole()
 
 const filters = computed(() => {
-    const baseFilter = auth.currentUser && auth.currentUser.uid
-        ? `uid:${auth.currentUser.uid} AND deleted:false` : ""
+    const baseFilter =
+        auth.currentUser && auth.currentUser.uid
+            ? `uid:${auth.currentUser.uid} AND deleted:false`
+            : ''
 
     if (!role.value || role.value == 'editor') {
         return baseFilter
     }
 
-    return "deleted:false"
-
+    return 'deleted:false'
 })
 
 const refineInput = (val: string, refine: (val: string) => void) => {
     refine(val)
 }
-const sortByOptions = [{
-    label: 'default.', value: 'invoices'
-},
-{
-    label: 'Created at desc.', value: 'invoices_createdAt_desc'
-},
-{
-    label: 'Created at asc.', value: 'invoices_createdAt_asc'
-},
-{
-    label: 'Name desc.', value: 'invoices_name_desc'
-},
-{
-    label: 'Name asc.', value: 'invoices_name_asc'
-},
-{
-    label: 'Customer Name desc.', value: 'invoices_customerName_desc'
-},
-{
-    label: 'Customer Name asc.', value: 'invoices_customerName_asc'
-},
-{
-    label: 'Code desc.', value: 'invoices_code_desc'
-},
-{
-    label: 'Code asc.', value: 'invoices_code_asc'
-}]
+const sortByOptions = [
+    {
+        label: 'default.',
+        value: 'invoices_createdAt_desc'
+    },
+    {
+        label: 'Created at desc.',
+        value: 'invoices_createdAt_desc'
+    },
+    {
+        label: 'Created at asc.',
+        value: 'invoices_createdAt_asc'
+    },
+    {
+        label: 'Name desc.',
+        value: 'invoices_name_desc'
+    },
+    {
+        label: 'Name asc.',
+        value: 'invoices_name_asc'
+    },
+    {
+        label: 'Customer Name desc.',
+        value: 'invoices_customerName_desc'
+    },
+    {
+        label: 'Customer Name asc.',
+        value: 'invoices_customerName_asc'
+    },
+    {
+        label: 'Code desc.',
+        value: 'invoices_code_desc'
+    },
+    {
+        label: 'Code asc.',
+        value: 'invoices_code_asc'
+    }
+]
 
-const format = (tiemstamp: number) => dayjs(new Date(tiemstamp)).format("DD/MM/YYYY")
+const format = (tiemstamp: number) => dayjs(new Date(tiemstamp)).format('DD/MM/YYYY')
 const changeSort = (value: string) => {
     console.log(sortBy)
     // const sortByEl: HTMLSelectElement | null = document.querySelector('.ais-SortBy-select')
@@ -86,13 +105,12 @@ const changeSort = (value: string) => {
     // sortByIndex.value = value
     // sortByEl.value = value;
     // sortByEl.dispatchEvent(new Event('change'));
-    flow.value = flow.value == "desc" ? 'asc' : 'desc'
+    flow.value = flow.value == 'desc' ? 'asc' : 'desc'
 }
 
 const onDeleteCancel = () => {
     invoiceId.value = null
     openDeleteModal.value = false
-
 }
 
 const OnDeleteContinue = async () => {
@@ -101,46 +119,55 @@ const OnDeleteContinue = async () => {
     await invoiceStore.remove(invoiceId.value)
     onDeleteCancel()
     toast.success('Invoice deleted', {
-        position: "top-center",
+        position: 'top-center',
         description: 'Invoice was deleted succesfully',
         closeButton: true
     })
-
 }
 
 const onInvoiceDelete = (id: string) => {
-    invoiceId.value = id;
+    invoiceId.value = id
     openDeleteModal.value = true
 }
 
-const columns = [{
-    name: "# Id",
-    property: 'code'
-},
-{
-    name: "Date:",
-    property: 'createdAt'
-},{
-    name: "Actions:",
-    property: 'actions'
-},]
-
-
+const columns = [
+    {
+        name: '# Id',
+        property: 'code'
+    },
+    {
+        name: 'Date:',
+        property: 'createdAt'
+    },
+    {
+        name: 'Actions:',
+        property: 'actions'
+    }
+]
 </script>
 <template>
-    <ais-instant-search :search-client="searchClient" index-name="invoices" class="flex flex-col" :routing="routing">
+    <ais-instant-search
+        :search-client="searchClient"
+        index-name="invoices"
+        class="flex flex-col"
+        :routing="routing"
+    >
         <ais-search-box class="mb-4">
             <template #default="{ currentRefinement, refine }">
                 <div class="relative w-full">
-   <Input :value="currentRefinement"
-                    @update:model-value="(val: string | number) => refineInput(val as string, refine)"
-                    placeholder="Search invoices..." class="bg-white pl-10"  />
+                    <Input
+                        :value="currentRefinement"
+                        @update:model-value="
+                            (val: string | number) => refineInput(val as string, refine)
+                        "
+                        placeholder="Search invoices..."
+                        class="bg-white pl-10"
+                    />
 
                     <Button variant="ghost" class="absolute left-0 top-0 z-50">
-                        <SearchIcon class="h-6 w-6"/>
+                        <SearchIcon class="h-6 w-6" />
                     </Button>
                 </div>
-
             </template>
         </ais-search-box>
         <Card>
@@ -151,17 +178,25 @@ const columns = [{
                         Manage your invoices, view details, and export them.
                     </CardDescription>
                 </div>
-                <ais-sort-by :items="sortByOptions" default:>
+                <ais-sort-by :items="sortByOptions" default-refinement="created_at_desc">
                     <template #default="{ items, currentRefinement, canRefine, refine }">
                         <div class="flex flex-start gap-2">
                             <Label>Sort:</Label>
-                            <Select :model-value="currentRefinement" @update:model-value="refine" ref="sortBy"
-                                :disabled="!canRefine">
+                            <Select
+                                :model-value="currentRefinement"
+                                @update:model-value="refine"
+                                ref="sortBy"
+                                :disabled="!canRefine"
+                            >
                                 <SelectTrigger class="w-[180px]">
                                     <SelectValue placeholder="Sort by ..." />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    <SelectItem v-for="item in items" :key="item.value" :value="item.value">
+                                    <SelectItem
+                                        v-for="item in items"
+                                        :key="item.value"
+                                        :value="item.value"
+                                    >
                                         {{ item.label }}
                                     </SelectItem>
                                 </SelectContent>
@@ -171,12 +206,9 @@ const columns = [{
                 </ais-sort-by>
             </CardHeader>
             <CardContent>
-                <ais-state-results v-slot="{ status, isSearchStalled, }">
-                    <div v-if="status === 'loading' || isSearchStalled">
-                        loading...
-                    </div>
-                    <div v-else class="hidden">
-                    </div>
+                <ais-state-results v-slot="{ status, isSearchStalled }">
+                    <div v-if="status === 'loading' || isSearchStalled">loading...</div>
+                    <div v-else class="hidden"></div>
                 </ais-state-results>
 
                 <ais-hits>
@@ -185,13 +217,13 @@ const columns = [{
                             <TableHeader>
                                 <TableRow>
                                     <TableHead v-for="col in columns" :key="col.name">
-                                        <div class="flex items-center gap-2 cursor-pointer"
-                                            @click="changeSort(`invoices_${col.property}_${flow}`)">
+                                        <div
+                                            class="flex items-center gap-2 cursor-pointer"
+                                            @click="changeSort(`invoices_${col.property}_${flow}`)"
+                                        >
                                             {{ col.name }}
                                         </div>
                                     </TableHead>
-
-
                                 </TableRow>
                             </TableHeader>
                             <TableBody v-if="items">
@@ -201,10 +233,9 @@ const columns = [{
                                 <TableRow v-for="item in items" :key="item.objectId">
                                     <TableCell class="w-1/3">
                                         <p class="text-wrap">
-                                               {{ item.code }} <br>
-                                        {{ item.name }}
+                                            {{ item.code }} <br />
+                                            {{ item.name }}
                                         </p>
-
                                     </TableCell>
 
                                     <TableCell class="w-1/3">
@@ -212,25 +243,33 @@ const columns = [{
                                     </TableCell>
                                     <TableCell class="w-1/3">
                                         <div class="flex items-center flex-col lg:flex-row">
-                                            <router-link :to="`/dashboard/invoices/${item.objectID}/edit`"
-                                                class="px-3 py-2">
+                                            <router-link
+                                                :to="`/dashboard/invoices/${item.objectID}/edit`"
+                                                class="px-3 py-2"
+                                            >
                                                 <Button>
                                                     <EditIcon class="h-4 w-4" />
                                                     Edit
                                                 </Button>
-
                                             </router-link>
-                                            <router-link tar :to="`/dashboard/invoices/${item.objectID}/print`"
-                                                target="_blank" rel="noopener noreferrer" class="px-3 py-2">
+                                            <router-link
+                                                tar
+                                                :to="`/dashboard/invoices/${item.objectID}/print`"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                class="px-3 py-2"
+                                            >
                                                 <Button>
                                                     <EyeIcon class="h-4 w-4" />
                                                     View
                                                 </Button>
-
                                             </router-link>
-                                            <Button variant="destructive" title="Remove invoice"
+                                            <Button
+                                                variant="destructive"
+                                                title="Remove invoice"
                                                 @click="onInvoiceDelete(item.objectID)"
-                                                class="0 opacity-75 hover:bg-red-200  hover:text-red-600 hover:opacity-100 ">
+                                                class="0 opacity-75 hover:bg-red-200 hover:text-red-600 hover:opacity-100"
+                                            >
                                                 <Trash :size="14" />
 
                                                 Delete
@@ -247,22 +286,28 @@ const columns = [{
             </CardContent>
             <CardFooter>
                 <ais-pagination class="w-full">
-                    <template #default="{ currentRefinement, nbPages, createURL, isLastPage, refine }">
+                    <template
+                        #default="{ currentRefinement, nbPages, createURL, isLastPage, refine }"
+                    >
                         <div class="flex flex-row justify-between items-center w-full">
-
                             <div class="text-sm">
                                 page {{ currentRefinement + 1 }} of {{ nbPages }}
                             </div>
                             <div class="flex gap-4 items-center">
-                                <a as="a" :href="createURL(currentRefinement - 1)"
-                                    @click.prevent="refine(currentRefinement - 1)">
+                                <a
+                                    as="a"
+                                    :href="createURL(currentRefinement - 1)"
+                                    @click.prevent="refine(currentRefinement - 1)"
+                                >
                                     <Button>
                                         <ChevronLeft />
                                     </Button>
                                 </a>
-                                <a :href="createURL(currentRefinement + 1)" :disabled="isLastPage"
-                                    @click.prevent="refine(currentRefinement + 1)">
-
+                                <a
+                                    :href="createURL(currentRefinement + 1)"
+                                    :disabled="isLastPage"
+                                    @click.prevent="refine(currentRefinement + 1)"
+                                >
                                     <Button>
                                         <ChevronRight />
                                     </Button>
@@ -273,7 +318,11 @@ const columns = [{
                 </ais-pagination>
             </CardFooter>
         </Card>
-        <InvoIceDeleteDialog :open="openDeleteModal" :loading="invoiceStore.deleting" @onCancel="onDeleteCancel"
-            @on-continue="OnDeleteContinue" />
+        <InvoIceDeleteDialog
+            :open="openDeleteModal"
+            :loading="invoiceStore.deleting"
+            @onCancel="onDeleteCancel"
+            @on-continue="OnDeleteContinue"
+        />
     </ais-instant-search>
 </template>
